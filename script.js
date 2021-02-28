6449a8190641b0bae7f8ff5fc340cc313c79f301
Laura Oberwetter <leoberwetter@gmail.com>
2021-02-28 16:07:36 -0600

    Third Commit

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ var answer1 = document.querySelector("#a1");
 var answer2 = document.querySelector("#a2");
 var answer3 = document.querySelector("#a3");
 var answer4 = document.querySelector("#a4");
-var answer5 = document.querySelector("#a5");
 var btn = document.querySelector(".btn");
 
 
@@ -22,21 +21,54 @@ startButton.addEventListener("click", startQuiz); //start game by clicking start
 
 
 function startQuiz() { //quiz start function
-    quizScreenEl.style.display = "inline"; //show the quiz screen
-    startScreenEl.style.display = "none"; //hide the start screen 
-    startButton.style.display = "none"; // hide start button
+  quizScreenEl.style.display = "inline"; //show the quiz screen
+  startScreenEl.style.display = "none"; //hide the start screen 
+  startButton.style.display = "none"; // hide start button
 
   //start the timer
 }
 
+// move to the next question on answer click
+var quizOptions = [ // array
+  { // object i = 0
+    question: "what is my name",
+    choice: ["Laura", "Lauren", "Laurie", "Laureli"],
+    answer: "Laura",
+  },
 
-btn.addEventListener("click", nextQuestion); // click any of the buttons, move to the next question
+  { // object i = 1
+    question: "what is my age",
+    choice: [27, 22, 31, 24],
+    answer: 24,
+  },
 
-function nextQuestion() { // make function to change the question and the answer choices
-  for (var i = 0; i < questions.length; i++) {
-    questionEl.innerHTML = questions[i];
+  { // object i = 2
+    question: "where am I from",
+    choice: ["Madison", "Chicago", "Milwaukee", "NYC"],
+    answer: "Madison",
   }
-}; 
+
+]
+
+// loop through options on click
+let i = 0;
+btn.addEventListener("click", () => {
+  
+  console.log(event.target.textContent); 
+  if (event.target.textContent === quizOptions[i].answer) {}
+
+  questionEl.textContent = quizOptions[i].question; // THEN changes question
+  answer1.textContent = quizOptions[i].choice[0];
+  answer2.textContent = quizOptions[i].choice[1];
+  answer3.textContent = quizOptions[i].choice[2];
+  answer4.textContent = quizOptions[i].choice[3];
+  console.log(quizOptions[i]);
+  
+  
+  i++;
+  
+
+});
 
 
 
@@ -57,23 +89,3 @@ function nextQuestion() { // make function to change the question and the answer
 
 
 
-var quizOptions = [ // array
-  { // objects
-    question: "what is my name",
-    choice: ["Laura", "Lauren", "Laurie", "Laureli"],
-    answer: "Laura",
-  },
-
-  {
-    question: "what is my age",
-    choice: [27, 22, 31, 24],
-    answer: 24,
-  },
-
-  {
-    question: "where am I from",
-    choice: ["Madison", "Chicago", "Milwaukee", "NYC"],
-    answer: "Madison",
-  }
-
-]
