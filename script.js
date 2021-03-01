12370e83284db984dacad9345d7bc901ecc8e79c
Laura Oberwetter <leoberwetter@gmail.com>
2021-02-28 20:49:01 -0600

    Fifth Commit

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var startButton = document.querySelector("#start-button");
-var timerElement = document.querySelector("#timer-count");
+
 // timer vars
-// content display
+var countdownEl = document.querySelector("#countdown");
+var seconds = 59;
+
+// content display vars
 var startScreenEl = document.querySelector("#start-screen");
 var quizScreenEl = document.querySelector("#quiz-screen");
+var endScreenEl = document.querySelector("#end-screen");
 var result = document.querySelector("#resultNotification");
-// Q and A
+
+// Q and A vars
 var questionEl = document.querySelector("#question");
 var answer1 = document.querySelector("#a1");
 var answer2 = document.querySelector("#a2");
@@ -13,25 +18,54 @@ var answer3 = document.querySelector("#a3");
 var answer4 = document.querySelector("#a4");
 var btn = document.querySelector(".btn");
 
+// functions
 
+function renderQuestion() {
+  questionEl.textContent = quizOptions[i].question; // THEN changes question
+  answer1.textContent = quizOptions[i].choice[0];
+  answer2.textContent = quizOptions[i].choice[1];
+  answer3.textContent = quizOptions[i].choice[2];
+  answer4.textContent = quizOptions[i].choice[3];
+}
 
-var timer;
-var timerCount;
+function countdownTimer() {
+  var interval = setInterval(function () {
+    countdownEl.innerHTML = "Seconds Left: " + seconds;
+    seconds--;
+    if (seconds === 0) {
+      clearInterval(interval);
+      countdownEl.innerHTML = "Time's Up!";
+      result.textContent = "Time's Up!";
+      result.style.color = "red";
+    }
+  }, 1000);
+
+}
+
+// START QUIZ
+endScreenEl.addEventListener("load", hideEnd);
+function hideEnd () {
+  endScreenEl.style.display = "none";
+}
 
 
 startButton.addEventListener("click", startQuiz); //start game by clicking start function
-
-
-function startQuiz() { //quiz start function
+function startQuiz() { //quiz start function initiated by start button
+  
   quizScreenEl.style.display = "inline"; //show the quiz screen
   startScreenEl.style.display = "none"; //hide the start screen 
   startButton.style.display = "none"; // hide start button
+  endScreenEl.style.display = "none"; // hide start button
 
-  //start the timer
+  renderQuestion() // populate questions and answer choices
+  console.log(quizOptions[i].answer + " is the corresponding answer(36)");
+
+  countdownTimer() // start countdown
 }
 
-// move to the next question on answer click
-var quizOptions = [ // array
+// QUESTIONS
+
+var quizOptions = [
   { // object i = 0
     question: "what is my name",
     choice: ["Laura", "Lauren", "Laurie", "Laureli"],
@@ -52,33 +86,37 @@ var quizOptions = [ // array
 
 ]
 
-// loop through options on click
+// CHECK ANSWER
+
 let i = 0;
 btn.addEventListener("click", () => {
-  
-  console.log(event.target.textContent); 
-  if (event.target.textContent === quizOptions[i].answer) {
+  console.log('ANSWER WAS CLICKED')
+
+  console.log(event.target.textContent + " was clicked");
+  if (event.target.textContent == quizOptions[i].answer) {
     result.textContent = "Correct!";
     result.style.color = "lawngreen";
   } else {
     result.textContent = "Incorrect";
     result.style.color = "red";
-    //deduct 10 seconds from the clock
+    seconds -= 10; //deduct 10 seconds from the clock
 
   }
 
-  questionEl.textContent = quizOptions[i].question; // THEN changes question
-  answer1.textContent = quizOptions[i].choice[0];
-  answer2.textContent = quizOptions[i].choice[1];
-  answer3.textContent = quizOptions[i].choice[2];
-  answer4.textContent = quizOptions[i].choice[3];
-  console.log(quizOptions[i]);
-  
-  
+  // INCREASE INDEX, MOVE TO NEXT QUESTION
+  console.log(i);
+
   i++;
-  
+  renderQuestion()
+  if (i > quizOptions.length) {
+    function showEnd () {
+      endScreenEl.style.display = "inline";
+    }
+  }
 
 });
+
+
 
 
 
